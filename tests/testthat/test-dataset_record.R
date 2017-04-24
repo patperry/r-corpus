571b2a8dc2db27d5e481da5bfaacf623cde9654d
Patrick Perry <patperry@gmail.com>
2017-04-24 17:56:49 +0100

    add test

---
+++ tests/testthat/test-dataset_record.R
@@ -0,0 +1,17 @@
+context("dataset_record")
+
+
+test_that("converting to list works", {
+    x <- as.integer(c(1, 1, 2, 3, 5))
+    y <- c("F", "i", "b", "b", "o")
+    file <- tempfile()
+    writeLines(paste0('{"x":', x, ',"y":"', y, '"}'), file)
+
+    ds <- read_json(file)
+    l <- as.list(ds)
+
+    expect_equal(l, list(x=x, y=as.text(y)))
+
+    rm("ds"); gc()
+    file.remove(file)
+})
