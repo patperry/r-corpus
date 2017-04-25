266e4d8ba359fe4e7091a0e63bc81dc5be6cb66c
Patrick Perry <patperry@gmail.com>
2017-04-25 17:03:46 -0400

    add text serialization test

--- tests/testthat/test-text.R
+++ tests/testthat/test-text.R
@@ -16,3 +16,29 @@ test_that("`as.data.frame` should work", {
     expect_equal(rownames(d), names(x))
     expect_equal(d[["x"]], as.text(x)) # drop names
 })
+
+
+test_that("serialization should work", {
+    x <- c("Once upon a time there were four little Rabbits,",
+	       "and their names were: Flopsy, Mopsy, Cottontail, and Peter.",
+	       "They lived with their Mother in a sandbank,",
+	       "underneath the root of a very big fir tree.",
+	       "'Now, my dears,' said old Mrs. Rabbit one morning,",
+	       "'you may go into the fields or down the lane,",
+	       "but don't go into Mr. McGregor's garden --",
+	       "your Father had an accident there;",
+	       "he was put in a pie by Mrs. McGregor.'")
+
+    file <- tempfile()
+    writeLines(paste0('"', x, '"'), file)
+    ds <- read_json(file)
+    text <- as.text(ds)
+
+    file2 <- tempfile()
+    saveRDS(text, file2)
+    text2 <- readRDS(file2)
+
+    expect_equal(text, text2)
+
+    rm("ds", "text", "text2"); gc(); file.remove(file, file2)
+})
