216ccd7b2045a3f2e72c2556867ccd5c5de2060f
Patrick Perry <patperry@gmail.com>
2017-04-27 09:35:12 -0400

    add 'flattened' version of sentences

--- src/text_proc.c
+++ src/text_proc.c
@@ -15,6 +15,7 @@
  */
 
 #include <assert.h>
+#include <stdlib.h>
 #include <string.h>
 #include "corpus/src/table.h"
 #include "corpus/src/text.h"
@@ -22,63 +23,138 @@
 #include "corpus/src/symtab.h"
 #include "corpus/src/sentscan.h"
 #include "corpus/src/wordscan.h"
+#include "corpus/src/xalloc.h"
 #include "rcorpus.h"
 
 
 SEXP sentences_text(SEXP sx)
 {
-	(void)sx;
-	return R_NilValue;
-	/*
-	SEXP ans, ans_i, prot, names, stext;
+	SEXP ans, handle, sources, pnames, psource, prow, pstart, pstop,
+	     ptable, source, row, start, stop, index, pname, names;
 	const struct text *text;
-	struct text *buf, *text_i;
+	struct text *sent, *sent1;
+	R_xlen_t *parent, *parent1;
 	struct sentscan scan;
-	R_xlen_t i, n, nbuf, nbuf_max;
+	R_xlen_t i, src, n, isent, nsent, nsent_max;
+	double r;
+	int j, off, len, iname;
 
-	PROTECT(stext = coerce_text(sx));
-	text = as_text(stext, &n);
-	prot = R_ExternalPtrProtected(stext);
+	PROTECT(sx = coerce_text(sx));
+	text = as_text(sx, &n);
+	sources = getListElement(sx, "sources");
+	ptable = getListElement(sx, "table");
+	psource = getListElement(ptable, "source");
+	prow = getListElement(ptable, "row");
+	pstart = getListElement(ptable, "start");
+	pstop = getListElement(ptable, "stop");
 
-	PROTECT(ans = allocVector(VECSXP, n));
-	names = names_text(stext);
-	setAttrib(ans, R_NamesSymbol, names);
+	pnames = names_text(sx);
 
-	buf = NULL;
-	nbuf_max = 0;
+	nsent = 0;
+	nsent_max = 256;
+	sent = xmalloc(nsent_max * sizeof(*sent));
+	parent = xmalloc(nsent_max * sizeof(*parent));;
+	if (sent == NULL || parent == NULL) {
+		free(sent);
+		free(parent);
+		error("memory allocation failure");
+	}
 
 	for (i = 0; i < n; i++) {
 		if (!text[i].ptr) {
-			SET_VECTOR_ELT(ans, i, alloc_na_text());
 			continue;
 		}
 
-		nbuf = 0;
 		sentscan_make(&scan, &text[i]);
-
 		while (sentscan_advance(&scan)) {
-			if (nbuf == nbuf_max) {
-				if (nbuf_max == 0) {
-					nbuf_max = 1;
-				} else {
-					nbuf_max = 2 * nbuf_max;
+			if (nsent == nsent_max) {
+				nsent_max = 2 * nsent_max;
+
+				sent1 = xrealloc(sent,
+						 nsent_max * sizeof(*sent));
+				if (!sent1) {
+					free(sent);
+					free(parent);
+					error("memory allocation failure");
 				}
-				buf = (void *)S_realloc((void *)buf, nbuf_max,
-							nbuf, sizeof(*buf));
+				sent = sent1;
+
+				parent1 = xrealloc(parent,
+						   nsent_max * sizeof(*parent));
+				if (!parent1) {
+					free(sent);
+					free(parent);
+					error("memory allocation failure");
+				}
+				parent = parent1;
 			}
 
-			buf[nbuf] = scan.current;
-			nbuf++;
+			sent[nsent] = scan.current;
+			parent[nsent] = i;
+			nsent++;
 		}
-
-		SET_VECTOR_ELT(ans, i, (ans_i = alloc_text(nbuf, prot)));
-		text_i = as_text(ans_i, NULL);
-		memcpy(text_i, buf, nbuf * sizeof(*buf));
 	}
 
-	UNPROTECT(2);
+	// free excess memory
+	sent = xrealloc(sent, nsent * sizeof(*sent));
+	parent = xrealloc(parent, nsent * sizeof(*parent));
+
+	PROTECT(source = allocVector(INTSXP, nsent));
+	PROTECT(row = allocVector(REALSXP, nsent));
+	PROTECT(start = allocVector(INTSXP, nsent));
+	PROTECT(stop = allocVector(INTSXP, nsent));
+	PROTECT(index = allocVector(INTSXP, nsent));
+
+	i = -1;
+	j = 0;
+	for (isent = 0; isent < nsent; isent++) {
+		if (parent[isent] != i) {
+			i = parent[isent];
+			j = 0;
+			src = INTEGER(psource)[i];
+			r = REAL(prow)[i];
+			off = INTEGER(pstart)[i];
+		}
+		len = (int)TEXT_SIZE(&text[i]);
+
+		INTEGER(source)[isent] = src;
+		REAL(row)[isent] = r;
+		INTEGER(start)[isent] = off;
+		INTEGER(stop)[isent] = off + (len - 1);
+		INTEGER(index)[isent] = j + 1;
+		j++;
+		off += len;
+	}
+
+	PROTECT(ans = alloc_text(sources, source, row, start, stop));
+	handle = getListElement(ans, "handle");
+	R_SetExternalPtrAddr(handle, sent);
+
+	i = -1;
+	j = 0;
+	if (pnames != R_NilValue) {
+		PROTECT(names = allocVector(STRSXP, nsent));
+		for (isent = 0; isent < nsent; isent++) {
+			if (parent[isent] != i) {
+				i = parent[isent];
+				j = 0;
+				pname = STRING_ELT(pnames, i);
+			}
+			// TODO concatenate pname + (j+1)
+			SET_STRING_ELT(names, isent, pname);
+			j++;
+		}
+
+		iname = findListElement(ans, "names");
+		SET_VECTOR_ELT(ans, iname, names);
+
+		UNPROTECT(1);
+	}
+
+	free(parent);
+
+	UNPROTECT(7);
 	return ans;
-	*/
 }
 
 
