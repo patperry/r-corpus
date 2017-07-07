a1ddde9285216b381b673629fcc02c48a0498d04
Patrick Perry <patperry@gmail.com>
2017-07-06 20:47:15 -0400

    expand test function

--- src/test.c
+++ src/test.c
@@ -3,6 +3,8 @@
 
 SEXP test_print(SEXP x)
 {
-	Rprintf("%s\n", translateChar(STRING_ELT(x, 0)));
+	Rprintf("native: '%s'\n", translateChar(STRING_ELT(x, 0)));
+	Rprintf("utf-8:  '%s'\n", translateCharUTF8(STRING_ELT(x, 0)));
+	Rprintf("raw:    '%s'\n", CHAR(STRING_ELT(x, 0)));
 	return R_NilValue;
 }
