29bd53eca422e1df6670ada62c9bac9b3a1650a5
Patrick Perry <patperry@gmail.com>
2017-07-13 15:52:28 -0400

    add term_frame tests

--- tests/testthat/test-term_matrix.R
+++ tests/testthat/test-term_matrix.R
@@ -170,3 +170,50 @@ test_that("'term_matrix' can select types ending in '.s", {
                  Matrix::sparseMatrix(i = 1, j = 1, x = 1,
                                       dimnames = list(NULL, "u.s")))
 })
+
+
+test_that("'term_frame' gives equivalent results to 'term_matrix'", {
+    text <- c(a="A rose is a rose is a rose.",
+              b="A Rose is red, a violet is blue!",
+              c="A rose by any other name would smell as sweet.")
+    x <- term_matrix(text)
+    tf <- term_frame(text)
+    xtf <- Matrix::sparseMatrix(i = as.integer(tf$text),
+                                j = as.integer(tf$term),
+                                x = tf$count,
+                                dimnames = list(levels(tf$text),
+                                                levels(tf$term)))
+    expect_equal(x, xtf)
+})
+
+
+test_that("'term_frame' gives equivalent results to 'term_matrix' no names", {
+    text <- c("A rose is a rose is a rose.",
+              "A Rose is red, a violet is blue!",
+              "A rose by any other name would smell as sweet.")
+    x <- term_matrix(text)
+    rownames(x) <- as.character(seq_along(text))
+    tf <- term_frame(text)
+    xtf <- Matrix::sparseMatrix(i = as.integer(tf$text),
+                                j = as.integer(tf$term),
+                                x = tf$count,
+                                dimnames = list(levels(tf$text),
+                                                levels(tf$term)))
+    expect_equal(x, xtf)
+})
+
+
+test_that("'term_frame' with group gives equivalent results to 'term_matrix'", {
+    text <- c(a="A rose is a rose is a rose.",
+              b="A Rose is red, a violet is blue!",
+              c="A rose by any other name would smell as sweet.")
+    g <- factor(c("X", "Y", "X"))
+    x <- term_matrix(text, group = g)
+    tf <- term_frame(text, group = g)
+    xtf <- Matrix::sparseMatrix(i = as.integer(tf$group),
+                                j = as.integer(tf$term),
+                                x = tf$count,
+                                dimnames = list(levels(tf$group),
+                                                levels(tf$term)))
+    expect_equal(x, xtf)
+})
