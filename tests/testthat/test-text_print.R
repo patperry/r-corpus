7c8d0dee9bda863d601726d80b49ee578e769d42
Patrick Perry <patperry@gmail.com>
2017-08-18 15:19:29 -0400

    switch back to quotes

--- tests/testthat/test-text_print.R
+++ tests/testthat/test-text_print.R
@@ -7,7 +7,7 @@ test_that("'print.text' works without names", {
     x <- as_text(LETTERS)
     expect_equal(strsplit(capture_output(print(x)), "\n")[[1]],
                  c(paste0(format(paste0("[", 1:20, "]"), justify = "right"),
-                          " ", LETTERS[1:20]),
+                          " \"", LETTERS[1:20], "\""),
                    "...  (26 entries total)"))
 })
 
@@ -19,6 +19,6 @@ test_that("'print.text' works with names", {
     x <- as_text(LETTERS, names = paste0("foo", 1:26))
     expect_equal(strsplit(capture_output(print(x)), "\n")[[1]],
                  c(paste0(format(names(x)[1:20], justify = "left"),
-                          " ", LETTERS[1:20]),
+                          " \"", LETTERS[1:20], "\""),
                    "...   (26 entries total)"))
 })
