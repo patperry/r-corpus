b976bc75c8d6ff39b5cdd2b8ca788c1d7b6541c9
Patrick Perry <patperry@gmail.com>
2017-08-24 12:55:06 -0400

    add test for gutenberg_corpus

---
+++ tests/testthat/test-gutenberg_corpus.R
@@ -0,0 +1,13 @@
+context("gutenberg_corpus")
+
+test_that("'gutenberg_corpus' can download Jules Verne in French", {
+    if (!identical(Sys.getenv("TRAVIS"), "true")) {
+        skip("Not on Travis")
+    }
+
+    data <- gutenberg_corpus(800, verbose = FALSE)
+    expect_equal(data$title, "Le Tour du Monde en 80 Jours")
+    expect_equal(data$author, "Jules Verne")
+    expect_equal(data$language, "French")
+    expect_equal(nchar(as.character(data$text)), 421335)
+})
